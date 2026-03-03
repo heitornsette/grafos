fe45c1a42ce0efe40e6bbac89270f64bffbd8448
Hns <51509684+heitornsette@users.noreply.github.com>
2026-03-02 23:00:53 -0300

    feat(lista-indexada): implementar geração dos arrays alfa e beta

--- src/logica/listaIndexada.js
+++ src/logica/listaIndexada.js
@@ -1,28 +1,56 @@
 function criarGrafos() {
   const arestas = {}
   const vertices = []
+
   function criarVertice(vertice) {
     vertices.push(vertice)
     arestas[vertice] = []
   }
+
   function criarLigacao(vertice1, vertice2) {
     arestas[vertice1].push(vertice2)
     arestas[vertice2].push(vertice1)
   }
+
   function display() {
     let grafico = ""
-    vertices.forEach(vertice => {
+    vertices.forEach((vertice) => {
       grafico += vertice + "->" + arestas[vertice].join(", ") + "\n"
     })
     console.log(grafico)
   }
-  return {criarVertice, criarLigacao, display}
+
+  return { criarVertice, criarLigacao, display, arestas, vertices }
 }
 
-const grafos = criarGrafos()
+function gerarArraysListaIndexada(estados, ligacoes) {
+  const grafo = criarGrafos()
 
-estados.forEach(estado => grafos.criarVertice(estado))
+  estados.forEach((estado) => grafo.criarVertice(estado))
+  ligacoes.forEach(([a, b]) => grafo.criarLigacao(a, b))
 
-ligacoes.forEach(([a, b]) => grafos.criarLigacao(a, b))
+  const alfa = []
+  const beta = []
 
-grafos.display()
+  grafo.vertices.forEach((vertice) => {
+    alfa.push(beta.length)
+
+    grafo.arestas[vertice].forEach((vizinho) => {
+      beta.push(vizinho)
+    })
+  })
+
+  alfa.push(beta.length)
+
+  return { alfa, beta}
+}
+
+function exibirListaIndexada() {
+  const { alfa, beta } = gerarArraysListaIndexada(estados, ligacoes)
+
+  console.log("Lista indexada gerada:")
+  console.log("Alfa:", alfa)
+  console.log("Beta:", beta)
+}
+
+exibirListaIndexada()
